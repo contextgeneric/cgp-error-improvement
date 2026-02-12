bf5a74b84ce98fe6d4add1faab27fcea717390ff
Soares Chen <soares.chen@maybevoid.com>
2026-02-12 22:33:07 +0100

    Prepare for minor error format improvement

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -1,7 +1,7 @@
 // pub mod base_area;
 // pub mod base_area_2;
 // pub mod scaled_area;
-// pub mod scaled_area_2;
+pub mod scaled_area_2;
 // pub mod density;
 // pub mod density_2;
 // pub mod density_3;
